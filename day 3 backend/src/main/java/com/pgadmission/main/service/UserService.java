42044e9d072008e6e7eeadf004d4ca559705dfc7
Abinaya Ayyappasamy <110535284+abi-29@users.noreply.github.com>
2023-10-05 14:51:54 +0530

    Add files via upload

---
+++ day 3 backend/src/main/java/com/pgadmission/main/service/UserService.java
@@ -0,0 +1,50 @@
+package com.pgadmission.main.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pgadmission.main.dto.response.UserResponse;
+import com.pgadmission.main.model.Role;
+import com.pgadmission.main.model.User;
+import com.pgadmission.main.repository.UserRepo;
+
+
+@Service
+
+public class UserService {
+      
+      @Autowired
+  	UserRepo userRepository;
+  	public User create(User users) {
+  		return userRepository.save(users);
+  	}
+  	public Optional<User> read(Long id){
+  		return userRepository.findById(id);
+  	}
+  	public User update(User users) {
+  		return userRepository.save(users);
+  		
+  	}
+  	public void delete(Long Id) {
+  		userRepository.deleteById(Id);
+  	}
+  	public List<UserResponse> getAllUsers() {
+        List<User> userList = userRepository.findAll();
+        return userList.stream()
+                .filter(user -> !user.getRole().equals(Role.ADMIN))
+                .map(this::mapUserToUserResponse)
+                .collect(Collectors.toList());
+    }
+  	private UserResponse mapUserToUserResponse(User user) {
+        return UserResponse.builder()
+                .uid(user.getUid())
+                .name(user.getName())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .build();
+    }
+}
